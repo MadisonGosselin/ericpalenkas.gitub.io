977cd6de1989c9554213eb9567e961525efb79c6
Madison Gosselin <86690087+MadisonGosselin@users.noreply.github.com>
2024-08-20 15:23:48 +0000

    finished dark mode

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const body = document.querySelector("body");
 const switcher = document.querySelector(".modeSwitcher");
 
-let mode = "light";
+let mode = localStorage.getItem("currMode");
+
+const loadDisplay = () => {
+    if (mode == "light") {
+        switcher.innerText = "🌙";
+        body.classList.remove("darkMode");
+    } else {
+        switcher.innerText = "☀️";
+        body.classList.add("darkMode");
+    }
+};
+loadDisplay();
 
 switcher.addEventListener("click", () => {
     if (mode === "light") {
         switcher.innerText = "☀️";
         body.classList.add("darkMode");
-        mode = "dark";
+        localStorage.setItem("currMode", "dark");
     } else {
         switcher.innerText = "🌙";
         body.classList.remove("darkMode");
-        mode = "light";
+        localStorage.setItem("currMode", "light");
     }
+    mode = localStorage.getItem("currMode");
 });
-
