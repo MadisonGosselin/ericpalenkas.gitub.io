cd040fea7afcb263e6cf7b3e40f68d7a97c0cdab
Madison Gosselin <86690087+MadisonGosselin@users.noreply.github.com>
2024-08-20 12:22:28 +0000

    adding dark mode

---
+++ script.js
@@ -0,0 +1,16 @@
+const body = document.querySelector("body");
+const switcher = document.querySelector(".modeSwitcher");
+
+let mode = "light";
+
+switcher.addEventListener("click", () => {
+    if (mode === "light") {
+        switcher.innerText = "☀️";
+        body.classList.add("darkMode");
+        mode = "dark";
+    } else {
+        switcher.innerText = "🌙";
+        body.classList.remove("darkMode");
+        mode = "light";
+    }
+});
