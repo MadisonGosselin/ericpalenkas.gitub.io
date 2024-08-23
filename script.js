88053091381e412fa68923db9522ab58df23f583
Madison Gosselin <86690087+MadisonGosselin@users.noreply.github.com>
2024-08-23 18:53:12 +0000

    added certs and employment

--- script.js
+++ script.js
@@ -26,3 +26,22 @@ switcher.addEventListener("click", () => {
     }
     mode = localStorage.getItem("currMode");
 });
+
+const collapsible = document.querySelectorAll(".collapsible");
+console.log(collapsible);
+
+collapsible.forEach((section) => {
+    section.addEventListener("click", () => {
+        section.classList.toggle("active");
+        var content = section.nextElementSibling;
+        if (content.style.display === "inline-block")
+            content.style.display = "none";
+        else content.style.display = "inline-block";
+    });
+});
+
+const test = document.querySelector(".certImg");
+test.addEventListener("click", () => {
+    console.log("here");
+});
+
